59fb478b985ce40335291baa13b3d6585455ddd6
Ahamed Aakif <mmahamedaakif@gmail.com>
2026-02-02 17:57:33 +0530

    feat : implement API fetching and display posts in scriptApi.js

---
+++ git.css/scriptApi.js
@@ -0,0 +1,11 @@
+let postsElement = document.getElementById("api-output");
+ 
+ 
+ fetch("https://jsonplaceholder.typicode.com/posts").then(response => response.json()).then(data => {
+     console.log(data);
+        data.forEach(post => {  
+            let postDiv = document.createElement("div");
+            postDiv.innerHTML = `<h3>${post.title}</h3><p>${post.body}</p><hr>`;
+            postsElement.appendChild(postDiv);
+        });
+     });
