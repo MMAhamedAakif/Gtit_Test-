a3e7d0164c337331932afbd085f205df3dd95cbb
Ahamed Aakif <mmahamedaakif@gmail.com>
2026-02-04 17:43:02 +0530

    feat: update scroll button styles and enhance scroll functionality in nextSection.js

--- nextSection.js
+++ nextSection.js
@@ -9,8 +9,23 @@
 </script> */}
 
 
-document.getElementById("scrollDown").onclick = () => {
-  document.getElementById("nextSection").scrollIntoView({
-    behavior: "smooth"
-  });
-};
+// Scroll Up button scrolls to previous section or up by 300px
+const scrollUpBtn = document.getElementById("scrollUp");
+const scrollDownBtn = document.getElementById("scrollDown");
+
+if (scrollUpBtn) {
+  scrollUpBtn.onclick = () => {
+    window.scrollBy({ top: -300, behavior: "smooth" });
+  };
+}
+
+if (scrollDownBtn) {
+  scrollDownBtn.onclick = () => {
+    const nextSection = document.getElementById("nextSection");
+    if (nextSection) {
+      nextSection.scrollIntoView({ behavior: "smooth" });
+    } else {
+      window.scrollBy({ top: 300, behavior: "smooth" });
+    }
+  };
+}
